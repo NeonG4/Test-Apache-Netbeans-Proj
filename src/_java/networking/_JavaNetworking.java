bb6686db8ce656d74524f32f5e43deed97c957cc
David Stall <davidstall312@gmail.com>
2025-09-30 09:44:59 -0700

    First Commit

---
+++ src/_java/networking/_JavaNetworking.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package _java.networking;
+/**
+ *
+ * @author 1117078
+ */
+public class _JavaNetworking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Rather than running this file, please either run the Client.java or Server.java file.");
+    }
+    
+}
